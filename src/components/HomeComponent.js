05eddcf1936f3414d60a5660a5253a2334bfe63d
Rakesh Shrestha <rakeshshrestha3222@gmail.com>
2020-11-27 00:49:20 +0545

    fourth commit

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,33 +1,39 @@
 import React from 'react'
-import { Container, } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Container, Row, Col } from 'react-bootstrap';
+import { IconContext } from 'react-icons';
+import { FaGithubSquare, FaLinkedin, FaStackOverflow } from "react-icons/fa";
 import styled from 'styled-components';
 
 
 const Styles = styled.div`
-background-color:blue;
+margin:auto;
 `
 function Home(){
     return(
         <Styles>
             <Container>
                 <div className="text-center">
-                    <p><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSz5WdXGof9-myjMjjvhv4wO-caGJ217YKYTQ&usqp=CAU" height='200px'
+                        <p><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSz5WdXGof9-myjMjjvhv4wO-caGJ217YKYTQ&usqp=CAU" height='200px'
                         width='200px' className="mt-5"/></p>
                         <h2><b>Rakesh Shrestha</b></h2>
                         <h3>Full Stack Web Developer</h3>
                         
                         <hr></hr>
                         <p>
-                        HTML | CSS | CSS | JavaScript | jQuery | React | ReactNative | Ruby on Rails<br>
-                        </br>
-                        <FontAwesomeIcon icon="user" />
-                        <FontAwesomeIcon icon="user" />
-                        <FontAwesomeIcon icon="user"/>
+                        HTML | CSS | CSS | JavaScript | jQuery | React | ReactNative | Ruby on Rails
                         </p>
-                        
+                       <p >
+                           <IconContext.Provider value={{ color:'grey', size:'3rem' }} >
+                                <Row className="justify-content-md-center">
+                                    <Col xs lg="1"><FaGithubSquare /></Col>
+                                    <Col xs lg="1"><FaLinkedin /></Col>
+                                    <Col xs lg="1"><FaStackOverflow /></Col> 
+                                </Row>     
+                           </IconContext.Provider>
+                        </p>
                 </div>
             </Container>
+            
         </Styles>
     )
 }
--- src/components/main/FooterComponent.js
+++ src/components/main/FooterComponent.js
@@ -3,14 +3,14 @@ import styled from 'styled-components';
 
 
 const Styles = styled.div`
-background-color:blue;
+margin:auto;
 `
 
 function Footer(){
     return(
         <Styles>
         <div className="text-center">
-            <p><b>Dedign and Developed By <a href="https://www.shrestharakesh.com.np">Rakesh Shrestha.</a></b></p>
+            <p><b>Design and Developed By <a href="https://www.shrestharakesh.com.np">Rakesh Shrestha.</a></b></p>
         </div>
         </Styles>
     )
