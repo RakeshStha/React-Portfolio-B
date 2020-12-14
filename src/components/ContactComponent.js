2dc207290c5543e6be5c693f32dd0ee2c5902429
Rakesh Shrestha <rakeshshrestha3222@gmail.com>
2020-12-14 23:38:51 +0545

    redux-form

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react'
 import { Container, Row, Col, Card, Button } from 'react-bootstrap';
+import Alert from 'react-bootstrap/Alert'
 import { IconContext } from 'react-icons';
+import {Checkbox} from 'react-input-checkbox';
 import { FaGithub, FaFacebook, FaLinkedin, FaStackOverflow } from "react-icons/fa";
 import styled from 'styled-components';
 import {Control, LocalForm, Errors} from 'react-redux-form';
@@ -13,31 +15,59 @@ const Styles = styled.div`
 }
 
 `
+
+
+
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => (val) && (val.length >= len);
+
+
+
+
+
 // for numbers
 //const isNumber = (val) => !isNaN(Number(val));
 const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
 
 
+
 class Contact extends Component{
     constructor(props){
-        super(props)
+        super(props);
+        this.state={
+            agree:false,
+           
+            
+        }
         
     }
     
+   handleAgree=()=>{
+    this.setState({agree: !this.state.agree});
+   }
    
     handlerSubmit(values){
+        if(this.state.agree){
         console.log(JSON.stringify(values));
         alert(JSON.stringify(values));
     }
+    else{
+        
+          console.log("Please agree our terms and condition")
+        
+    }
+    }
+    
     
     
     render(){
+        
+       
     return(
         <Styles>
-        <Container className="m-auto">
+        <Container >
+            
             <Row className=" mt-5">
                 <Col sm={6}>
                     <div className="mt-5">
@@ -136,18 +166,69 @@ class Contact extends Component{
                     </Row><br></br>
                     <Row className="form-group">
                         <Col>
-                        <div className="form-check">
-                                <Control.checkbox
-                                        model=".agree"
-                                        name="agree"
-                                        className="form-check-input"
-                                        />
-                                {' '}
-                                <strong>I accept the Terms of Service</strong>
-                          </div>
+                        <Control type="password" model=".password" 
+                        name="password"
+                        className="form-control"
+                        placeholder="Enter your password" 
+                        validators={{
+                            required, 
+                                passwordsMatch: (value) => vals.password === vals.conPassword,
+                              
+                        }}
+                        
+                       />
+                       <Errors
+                            className="text-danger"
+                            model=".password"
+                            show="touched"
+                            messages={{
+                                required: 'Required',
+                                
+                                passwordsMatch: 'Password doesnot match'
+                            }}
+                            
+                    
+                       />
+                       
                         </Col>
                     </Row><br></br>
-                    <Button type="submit" className="rounded-pill bg-secondary">Submit</Button>
+                    <Row className="form-group">
+                        <Col>
+                        <Control type="password" model=".conpassword" 
+                        name="conpassword"
+                        className="form-control"
+                        placeholder="Please confirm your password" 
+                        validators={{
+                            required
+                        }}
+                       />
+                       <Errors
+                            className="text-danger"
+                            model=".conpassword"
+                            show="touched"
+                            messages={{
+                                required: 'Required',
+                                
+                                passwordsMatch: 'Password doesnot match'
+                            }}
+                            
+                    
+                       />
+                        </Col>
+                    </Row><br></br>
+                    <Row className="form-group">
+                        <Col>
+                            <Checkbox name="agree" value={this.state.agree} onChange={this.handleAgree}
+                            
+                            > <strong>I accepts the terms and condition.</strong></Checkbox>
+                            
+                        </Col>
+                        
+                    </Row>
+                    <Alert variant="info" >Note: Please agree our terms and condition to proceed forward.<br></br>
+                            Thank you!</Alert>
+                    <br></br>
+                    <Button type="submit" className="rounded-pill bg-secondary" disabled={!(this.state.agree)}>Submit</Button>
                     </LocalForm>
                     </Card.Body>
                     </div>
